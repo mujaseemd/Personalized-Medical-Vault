751a6aee1f94e0ab71435ed122217a8773805d2e
mujaseemd <mujaseemd09@gmail.com>
2025-04-24 11:44:24 +0530

    Update ipfs.js

--- client/src/ipfs.js
+++ client/src/ipfs.js
@@ -1,7 +1,7 @@
 import { create } from 'ipfs-http-client';
 
 const projectId = '34d6561de15d07716020';
-const projectSecret = '1fc721d0b8fce8edc12eb93e8eb72965abf831b462304238cef3ff28bcf94b66';
+const projectSecret = 'Secret_key';
 const auth = 'Basic ' + btoa(`${projectId}:${projectSecret}`);
 
 const ipfs = create({
